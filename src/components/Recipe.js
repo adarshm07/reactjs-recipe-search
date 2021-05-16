8307d7bdde8c99cf6247983687f76643b0fbea19
Adarsh M <adarshm954@gmail.com>
2021-05-16 20:46:00 +0530

    Add files via upload

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
-const Recipe = ({ label,type,img }) => {
+const Recipe = ({ label,type,img, link }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <CardBody>
-        <img src={ img }></img>
-        <h1>{label}</h1>
-        <p>{type}</p>
-        <Button variant="primary">Read More</Button>
-      </CardBody>
-    </Card>
+    <Container>
+      <Card style={{ width: "100%" }}>
+        <CardBody>
+          <img className="recipe__img" src={ img } style={{ width: "100%" }}></img>
+          <h2 className="recipe__name">{label}</h2>
+          <p>{type}</p>
+          <a href={link}><Button className="btn-primary success btn-sm">Read More</Button></a>
+        </CardBody>
+      </Card>
+    </Container>
   );
 };
 
